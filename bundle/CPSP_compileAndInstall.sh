81b5e6e4bcffabd08d5d1ee3a038d74fff35f65a
martin-mann <qyu@gmx.de>
2019-04-30 16:42:05 +0200

    bundle files

---
+++ bundle/CPSP_compileAndInstall.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+VERSION_BIU=2.3.5
+VERSION_GECODE=1.3.0.1
+VERSION_CPSP=2.4.7
+
+MAKE_JOBS=4
+
+#########################################
+echo " set up install pathes";
+#########################################
+
+export INSTALL_PATH=$HOME/CPSP-bundle-$VERSION_CPSP
+export INSTALL_CPSP_COREDB=$HOME/CPSP_cores
+
+#########################################
+# function for error handling
+#########################################
+
+function error_exit
+{
+    echo ${1:-"Unknown Error"} 1>&2
+    exit 1
+}
+
+
+#########################################
+echo " make and install Gecode library";
+#########################################
+
+tar -xzf gecode-$VERSION_GECODE.tar.gz || error_exit "Error while unpacking gecode-$VERSION_GECODE.tar.gz";
+cd gecode-$VERSION_GECODE;
+CXXFLAGS="-fpermissive -Wnonnull-compare" ./configure --enable-static --disable-shared --disable-set-vars --disable-examples --prefix=$INSTALL_PATH || (cd ..; error_exit "Configure of gecode-$VERSION_GECODE failed!");
+make -j $MAKE_JOBS || (cd ..; error_exit "Compilation of gecode-$VERSION_GECODE failed!");
+make install || (cd ..; error_exit "Installation of gecode-$VERSION_GECODE failed!");
+cd ..;
+
+#########################################
+echo " make and install BIU library";
+#########################################
+
+tar -xzf biu-$VERSION_BIU.tar.gz || error_exit "Error while unpacking biu-$VERSION_BIU.tar.gz";
+cd biu-$VERSION_BIU;
+CXXFLAGS="-fpermissive" ./configure --prefix=$INSTALL_PATH || (cd ..; error_exit "Configure of biu-$VERSION_BIU failed!");
+make -j $MAKE_JOBS || (cd ..; error_exit "Compilation of biu-$VERSION_BIU failed!");
+make install || (cd ..; error_exit "Installation of biu-$VERSION_BIU failed!");
+cd ..;
+
+#########################################
+echo " make and install CPSP package";
+#########################################
+
+tar -xzf cpsp-$VERSION_CPSP.tar.gz || error_exit "Error while unpacking cpsp-$VERSION_CPSP.tar.gz";
+cd cpsp-$VERSION_CPSP;
+./configure --enable-static-linking --with-BIU=$INSTALL_PATH --with-gecode=$INSTALL_PATH --prefix=$INSTALL_PATH || (cd ..; error_exit "Configure of cpsp-$VERSION_CPSP failed!");
+make -j $MAKE_JOBS || (cd ..; error_exit "Compilation of cpsp-$VERSION_CPSP failed!");
+make install || (cd ..; error_exit "Installation of cpsp-$VERSION_CPSP failed!");
+cd ..;
+
+#########################################
+echo " unzip Hcore data and install";
+#########################################
+
+mkdir -p $INSTALL_CPSP_COREDB || error_exit "Cannot create CPSP H-core folder";
+tar -xzf CPSP_CoreDB.cub.all.tar.gz || error_exit "Error while unpacking CPSP_CoreDB.cub.all.tar.gz";
+mv -f Cubic $INSTALL_CPSP_COREDB;
+tar -xzf CPSP_CoreDB.fcc.all.tar.gz || error_exit "Error while unpacking CPSP_CoreDB.fcc.all.tar.gz";
+mv -f Fcc $INSTALL_CPSP_COREDB;
+
+#########################################
+echo " run HPstruct example";
+#########################################
+
+$INSTALL_PATH/bin/HPstruct -seq=HPPPHPHHHPPHHHHPHPHHPHPH -lat=CUB -allbest -maxSol=20 -distMode=MOVE -distMin=6 -dbPath=$INSTALL_CPSP_COREDB -normalize || error_exit "Test run of HPstruct failed!";
+
+#########################################
+echo " all done ...";
+#########################################
+
+
---
+++ bundle/README.txt
@@ -0,0 +1,16 @@
+
+To compile and install the CPSP package and its dependencies just run the 
+ ./CPSP_compileAndInstall.sh 
+bash shell script. 
+
+The script will also unpack and install various H-cores for 3D cubic lattice.
+
+NOTE: 
+You have to adapt the pathes in the script if you dont want to install 
+the package and H-cores into subfolders of your home directory! 
+This was set to avoid the need for root privileges for the installation process.
+
+In case of questions or problems dont hesitate to contact me
+
+  Martin Raden  --  http://www.bioinf.uni-freiburg.de/~mmann/
+
